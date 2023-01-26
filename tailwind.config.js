82dfba3a2ca832eabb7dcae098c30a476f8ffc3e
oscaem <48035650+oscaem@users.noreply.github.com>
2023-01-26 02:47:04 +0000

    main

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
